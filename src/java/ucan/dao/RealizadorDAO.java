6ea86a5694edb1b58c5bc27a9e612bf112be98d3
YuriDomingos <josemarlabareda@gmail.com>
2022-01-01 09:48:37 +0100

    Trabalhando com a nacionalidade

--- src/java/ucan/dao/RealizadorDAO.java
+++ src/java/ucan/dao/RealizadorDAO.java
@@ -5,10 +5,43 @@
  */
 package ucan.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import ucan.modelo.RealizadorModelo;
+import ucan.utils.Conexao;
+
 /**
  *
- * @author yuri
+ * @author : Yuri Domingos 
+ * Data    : 01 - 01 - 2021
+ * Objectivo : Construir o realizador DAO 
  */
 public class RealizadorDAO {
+    
+    
+    
+    public static boolean cadastrarRealizador(RealizadorModelo realizadorModelo)
+    {
+        String insert = "INSERT INTO public.realizador VALUES (DEFAULT, ?, ?);";
+        
+        try
+        {
+            Connection con = Conexao.abrirConexao();
+            PreparedStatement ps = con.prepareStatement(insert);
+            
+            ps.setInt(1, realizadorModelo.getPk_Realizador());
+            ps.setInt(2, realizadorModelo.getFk_nacionalidade());
+            ps.execute();
+            ps.close();
+            
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("Erro ao inserir os dados "+ex.toString());
+        }
+        
+        return false;
+    }
     
 }
--- src/java/ucan/modelo/RealizadorModelo.java
+++ src/java/ucan/modelo/RealizadorModelo.java
@@ -7,8 +7,49 @@ package ucan.modelo;
 
 /**
  *
- * @author yuri
+ * @author : Yuri Domingos
+ * Data    : 01 - 01 - 202
+ * Objectivo : Construir o realizador modelo 
  */
-public class RealizadorModelo {
+public class RealizadorModelo 
+{
+    
+    private int pk_Realizador, fk_pessoa, fk_nacionalidade;
+
+    public RealizadorModelo() {
+    }
+
+    public RealizadorModelo(int pk_Realizador, int fk_pessoa, int fk_nacionalidade) {
+        this.pk_Realizador = pk_Realizador;
+        this.fk_pessoa = fk_pessoa;
+        this.fk_nacionalidade = fk_nacionalidade;
+    }
+
+    public int getPk_Realizador() {
+        return pk_Realizador;
+    }
+
+    public void setPk_Realizador(int pk_Realizador) {
+        this.pk_Realizador = pk_Realizador;
+    }
+
+    public int getFk_pessoa() {
+        return fk_pessoa;
+    }
+
+    public void setFk_pessoa(int fk_pessoa) {
+        this.fk_pessoa = fk_pessoa;
+    }
+
+    public int getFk_nacionalidade() {
+        return fk_nacionalidade;
+    }
+
+    public void setFk_nacionalidade(int fk_nacionalidade) {
+        this.fk_nacionalidade = fk_nacionalidade;
+    }
+    
+    
+    
     
 }
