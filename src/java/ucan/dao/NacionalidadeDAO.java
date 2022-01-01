a3bd808ed95f6689b8385fbb99e07fee286387d5
YuriDomingos <josemarlabareda@gmail.com>
2022-01-01 08:28:35 +0100

    Trabalhando com a nacionalidade

--- src/java/ucan/dao/NacionalidadeDAO.java
+++ src/java/ucan/dao/NacionalidadeDAO.java
@@ -5,10 +5,50 @@
  */
 package ucan.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import ucan.modelo.NacionalidadeModelo;
+import ucan.utils.Conexao;
+
 /**
  *
- * @author yuri
+ * @author Yuri Domingos 
+ * Data    : 01 - 01 - 20202
+ * Objectivo : Construir a nacionalidade modelo 
  */
 public class NacionalidadeDAO {
+    
+    
+    public ArrayList<NacionalidadeModelo> listarNacionalidade()
+    {
+        
+        ArrayList<NacionalidadeModelo> lista = new ArrayList<>();
+        String query = "SELECT *FROM nacionalidade;";
+        
+        try
+        {
+            Connection con = Conexao.abrirConexao();
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            
+            while( rs.next())
+            {
+                NacionalidadeModelo nacionalidadeM = new NacionalidadeModelo();
+                nacionalidadeM.setPk_Nacionalidade(rs.getInt(1));
+                nacionalidadeM.setDescricao(rs.getString(2));
+                lista.add(nacionalidadeM);
+            }
+            
+        }catch(SQLException ex)
+        {
+            System.out.println("ERRO "+ex.toString());
+        }
+        
+        return lista;
+        
+    }
     
 }
