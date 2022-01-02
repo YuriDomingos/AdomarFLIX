af50fbb21da2221a00b5acc65c6788ac3a87f991
YuriDomingos <josemarlabareda@gmail.com>
2022-01-02 09:53:55 +0100

    Fazendo a parte do filme

---
+++ src/java/ucan/servlets/CadastroFilme.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ucan.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Yuri Domingos
+ * Data   : 02 - 01 - 2021
+ * Objectivo : Cadastro de filme 
+ * 
+ */
+@WebServlet(name = "CadastroFilme", urlPatterns = {"/CadastroFilme"})
+
+public class CadastroFilme extends HttpServlet 
+{
+
+   
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+  
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+   
+
+}
